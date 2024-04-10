2cdf2d53caba17cd99ba8bbe79228e58b2caef0a
LemonP33l <166410182+LemonP33l@users.noreply.github.com>
2024-04-10 16:25:28 +0100

    Add files via upload

--- cursus/fts/ft_tolower.c
+++ cursus/fts/ft_tolower.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eduferna <eduferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/10 15:02:16 by eduferna          #+#    #+#             */
-/*   Updated: 2024/04/10 16:09:33 by eduferna         ###   ########.fr       */
+/*   Created: 2024/04/10 15:42:51 by eduferna          #+#    #+#             */
+/*   Updated: 2024/04/10 16:06:20 by eduferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-If char c is lowercase, return the uppercase equivalent;
+If char c is uppercase, return the lowercase equivalent;
 Ascii table has a 32 char difference between upper and lower equivalents;
 */
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 	{
-		c = c - 32;
+		c = c + 32;
 	}
 	return (c);
 }
